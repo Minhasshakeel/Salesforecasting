3491f52820e3eac5eda3c47308673f8172d00c64
Minhas Shakeel <111732636+Minhasshakeel@users.noreply.github.com>
2026-03-10 04:15:08 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -5,9 +5,7 @@ import joblib
 st.set_page_config(page_title="Sales Forecasting", layout="centered")
 st.title("📊 Sales Forecasting App")
 
-# ===============================
 # Load Model
-# ===============================
 try:
     model = joblib.load("lightgbm_model.pkl")
 except:
@@ -16,9 +14,7 @@ except:
 
 st.success("Model Loaded Successfully ✅")
 
-# ===============================
 # User Inputs (ONLY 7 FEATURES)
-# ===============================
 
 store_id = st.number_input("StoreID_encoded", min_value=0)
 product_id = st.number_input("ProductID_encoded", min_value=0)
@@ -28,18 +24,12 @@ lag7 = st.number_input("Lag7 (Last Week Same Day Sales)", min_value=0.0)
 
 date = st.date_input("Select Date")
 
-# ===============================
 # Feature Engineering
-# ===============================
-
 day = date.day
 month = date.month
 weekday = date.weekday()
 
-# ===============================
 # Create Input DataFrame
-# ===============================
-
 input_df = pd.DataFrame([[
     store_id,
     product_id,
@@ -58,10 +48,7 @@ input_df = pd.DataFrame([[
     'Weekday'
 ])
 
-# ===============================
 # Prediction
-# ===============================
-
 if st.button("Predict Sales"):
     prediction = model.predict(input_df)[0]
     st.success(f"📈 Predicted Units Sold: {round(prediction,2)}")
